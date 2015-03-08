d3acf1f3ce922f09b76522341a3fcf6af4cab6d6
Paul Davis <paulandcindy@gmail.com>
2015-03-08 00:54:24 -0500

    Initial Commit

---
+++ js/tip-calculator.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+function myFunction() {
+  var billTotal = document.getElementById("billTotal");
+  var tipPercent=  document.getElementById("tipPercent");
+    tipPercent=(tipPercent*.01);
+    var splitNumber = document.getElementById("splitNum.value");
+
+
+
+  console.log("drop down value", splitNumber);
+  console.log("here is the bill total",billTotal);
+
+
+
+
+
+
+
+
+
+    // var text = "";
+    // var i;
+    // for (i = 0; i < x.length ;i++) {
+    //     text += x.elements[i].value + "<br>";
+    // }
+    // document.getElementById("demo").innerHTML = text;
+}
