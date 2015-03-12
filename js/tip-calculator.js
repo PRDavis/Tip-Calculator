3c37785a40203e920bd1dfd858502cfbe9c03f44
Paul Davis <paulandcindy@gmail.com>
2015-03-11 21:47:35 -0400

    worked on making this design more responsive

--- js/tip-calculator.js
+++ js/tip-calculator.js
@@ -5,47 +5,38 @@ function myFunction() {
   var tipPercent=  document.getElementById("tipPercent").value;
   tipPercent=(tipPercent*.01);
   var splitNumber = document.getElementById("splitNum").value;
-
   var amount=billTotal;
 
   //do you need to round up ?
 
-     if(((splitNumber*1 >1) && (amount%splitNumber)!==0)){
-       amount=(amount/splitNumber)+.01;
-     };
-  console.log("here is the amount before currency function : ", amount);
+  if((splitNumber*1 >1) && ((parseInt((amount/splitNumber)*100)*splitNumber) < amount)){
+      amount=(amount/splitNumber)+.01;
+    }   else{
+    amount=amount/splitNumber;
+  };
+
   amount=toCurrency(amount);
-console.log("here is the amount after currency function : ", amount);
+
 
   var tipOutAmount=amount*tipPercent;
 
   tipOutAmount=toCurrency(tipOutAmount);
-  console.log("here is the tip after currency function : ", tipOutAmount);
+
   var totalWithTip=amount*1+tipOutAmount*1;
   totalWithTip=toCurrency(totalWithTip);
-  console.log("here is the total with tip : ", amount,"+",tipOutAmount,"=",totalWithTip);
+
   document.getElementById("amount").innerHTML="$"+amount;
   document.getElementById("tipOutAmount").innerHTML="$"+tipOutAmount;
   document.getElementById("totalWithTip").innerHTML="$"+totalWithTip;
 
-return ;
+  return ;
 }
 
 function toCurrency(raw){
   raw=(parseInt(raw*100))/100;
   raw=raw.toFixed(2)
   return raw;
-  }
+}
 // TODO - change the rounding formula to get rid of erroneous uprounding
 // TODO - finish the responsive work with bootstrap
 // TODO - upload to gh pages and test on mobile devices
-
-// <input type="number" id="billTotal"
-//
-// <input type="number" id="tipPercent"
-//
-// <select id="splitNum">
-//
-// <output id="amount">
-//
-// <output id="tipOutAmount">
