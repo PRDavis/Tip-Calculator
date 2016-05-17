047d6140d62bb6913d16682fb55e163fb1cb7663
Paul Davis <paulandcindy@gmail.com>
2016-05-16 23:56:13 -0400

    refactor: make new data model
    Make new data model for check and instantiate a new check object.

--- js/tip-calculator.js
+++ js/tip-calculator.js
@@ -1,4 +1,22 @@
-var theCheck = {
+var theCheck = function(){
+self = this;
+
+
+
+
+// this is the data model for the check
+
+  var Check = function(){
+    this.total =ko.observable("20.00");
+    this.tipPercent = ko.observable(20);
+    this.split = ko.observable;
+    this.amount = ko.computed;
+    this.tipAmount = ko.computed;
+    this.totalWithTip = ko.coputed;
+  };
+// instantiate a new check object
+var ourCheck = new Check();
+console.log('here is ourCheck: ', ourCheck);
 
 
 
@@ -7,50 +25,46 @@ var theCheck = {
 
 
 
+  //
+  // function myFunction() {
+  //   var billTotal = document.getElementById("billTotal").value;
+  //   var tipPercent=  document.getElementById("tipPercent").value;
+  //   tipPercent=(tipPercent / 100 );
+  //   var splitNumber = document.getElementById("splitNum").value;
+  //   var amount=billTotal;
+  //   ////////////////////////////////////
+  //   // @ TODO do you need to round up ?
+  //   ////////////////////////////////////
+  //   if((splitNumber * 1 > 1) && ((parseInt((amount/splitNumber)*100)*splitNumber) < amount)){
+  //     amount=(amount/splitNumber)+ 0.01;
+  //   }   else{
+  //     amount=amount/splitNumber;
+  //   }
+  //   amount=toCurrency(amount);
+  //   var tipOutAmount=amount*tipPercent;
+  //   tipOutAmount=toCurrency(tipOutAmount);
+  //   var totalWithTip=amount*1+tipOutAmount*1;
+  //   totalWithTip=toCurrency(totalWithTip);
+  //   document.getElementById("amount").innerHTML="$"+amount;
+  //   document.getElementById("tipOutAmount").innerHTML="$"+tipOutAmount;
+  //   document.getElementById("totalWithTip").innerHTML="$"+totalWithTip;
+  //   return ;
+  // }
+  //
+  //
+  // function toCurrency(raw){
+  //   raw=(parseInt(raw*100))/100;
+  //   raw=raw.toFixed(2);
+  //   return raw;
+  // }
+  // @TODO refactor using knockoutjs for better view updates
 
 
-
-
-
-function myFunction() {
-  var billTotal = document.getElementById("billTotal").value;
-  var tipPercent=  document.getElementById("tipPercent").value;
-  tipPercent=(tipPercent / 100 );
-  var splitNumber = document.getElementById("splitNum").value;
-  var amount=billTotal;
-  ////////////////////////////////////
-  // @ TODO do you need to round up ?
-  ////////////////////////////////////
-  if((splitNumber * 1 > 1) && ((parseInt((amount/splitNumber)*100)*splitNumber) < amount)){
-    amount=(amount/splitNumber)+ 0.01;
-  }   else{
-    amount=amount/splitNumber;
-  }
-  amount=toCurrency(amount);
-  var tipOutAmount=amount*tipPercent;
-  tipOutAmount=toCurrency(tipOutAmount);
-  var totalWithTip=amount*1+tipOutAmount*1;
-  totalWithTip=toCurrency(totalWithTip);
-  document.getElementById("amount").innerHTML="$"+amount;
-  document.getElementById("tipOutAmount").innerHTML="$"+tipOutAmount;
-  document.getElementById("totalWithTip").innerHTML="$"+totalWithTip;
-  return ;
-}
-
-
-function toCurrency(raw){
-  raw=(parseInt(raw*100))/100;
-  raw=raw.toFixed(2);
-  return raw;
-}
-// @TODO refactor using knockoutjs for better view updates
-
-
-// TODO - change the rounding formula to get rid of erroneous uprounding
-// TODO - finish the responsive work with bootstrap
-// TODO - upload to gh pages and test on mobile devices
+  // TODO - change the rounding formula to get rid of erroneous uprounding
+  // TODO - finish the responsive work with bootstrap
+  // TODO - upload to gh pages and test on mobile devices
 
 
 };
 
-$(ko.applyBindings(theCheck));
+$(ko.applyBindings(theCheck()));
