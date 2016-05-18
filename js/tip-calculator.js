080603239122efb52153daa54d8051e7c2f5073d
Paul Davis <paulandcindy@gmail.com>
2016-05-17 23:30:13 -0400

    refactor: Rewrite js code to be part of viewmodel
    Rewrite javascript code to be part of the knockoutsjs view model.
    Improved math rounding. Add html bindings.

--- js/tip-calculator.js
+++ js/tip-calculator.js
@@ -4,61 +4,20 @@ function theCheck(){
 // this is the data model for the check
 
 
-    this.total =ko.observable("22.30");
-    this.tipPercent = ko.observable("17.5");
-    this.split = ko.observable;
-    this.amount = ko.computed;
-    this.tipAmount = ko.computed;
-    this.totalWithTip = ko.coputed;
-
-
-
-
-
-
-
-
-
-  //
-  // function myFunction() {
-  //   var billTotal = document.getElementById("billTotal").value;
-  //   var tipPercent=  document.getElementById("tipPercent").value;
-  //   tipPercent=(tipPercent / 100 );
-  //   var splitNumber = document.getElementById("splitNum").value;
-  //   var amount=billTotal;
-  //   ////////////////////////////////////
-  //   // @ TODO do you need to round up ?
-  //   ////////////////////////////////////
-  //   if((splitNumber * 1 > 1) && ((parseInt((amount/splitNumber)*100)*splitNumber) < amount)){
-  //     amount=(amount/splitNumber)+ 0.01;
-  //   }   else{
-  //     amount=amount/splitNumber;
-  //   }
-  //   amount=toCurrency(amount);
-  //   var tipOutAmount=amount*tipPercent;
-  //   tipOutAmount=toCurrency(tipOutAmount);
-  //   var totalWithTip=amount*1+tipOutAmount*1;
-  //   totalWithTip=toCurrency(totalWithTip);
-  //   document.getElementById("amount").innerHTML="$"+amount;
-  //   document.getElementById("tipOutAmount").innerHTML="$"+tipOutAmount;
-  //   document.getElementById("totalWithTip").innerHTML="$"+totalWithTip;
-  //   return ;
-  // }
-  //
-  //
-  // function toCurrency(raw){
-  //   raw=(parseInt(raw*100))/100;
-  //   raw=raw.toFixed(2);
-  //   return raw;
-  // }
-  // @TODO refactor using knockoutjs for better view updates
-
-
-  // TODO - change the rounding formula to get rid of erroneous uprounding
-  // TODO - finish the responsive work with bootstrap
-  // TODO - upload to gh pages and test on mobile devices
-
-
+    this.total =ko.observable("27.58");
+    this.tipPercent = ko.observable("20");
+    this.split = ko.observable("1");
+    this.amount = ko.computed(function(){
+    var unformattedAmount = this.total() / this.split();
+    return (Math.floor(unformattedAmount * 100)/100).toFixed(2);
+    }, this);
+    this.tipAmount = ko.computed(function(){
+    var unformattedTipAmount = this.amount() * this.tipPercent() * 0.01;
+    unformattedTipAmount = (Math.round(unformattedTipAmount*100))/100;
+    return (Math.floor(unformattedTipAmount * 100)/100).toFixed(2);
+    }, this);
+    this.totalWithTip = ko.computed(function(){
+    return (Number(this.amount()) + Number(this.tipAmount())).toFixed(2);
+    }, this);
 }
-
 ko.applyBindings(theCheck());
