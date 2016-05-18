2c9668c1c271387336934269a25b19df0ca3fe23
Paul Davis <paulandcindy@gmail.com>
2016-05-18 00:15:10 -0400

    refactor: Remove input box spinners
    Remove webkit input box spinners to make formatting uniform.

--- js/tip-calculator.js
+++ js/tip-calculator.js
@@ -10,17 +10,13 @@ function theCheck(){
   this.amount = ko.computed(function(){
     var unformattedAmount = this.total() / this.split();
     var formattedAmount = (Math.floor(unformattedAmount * 100)/100).toFixed(2);
-    console.log('here is the type of formattedAmount: ', typeof formattedAmount);
     var numericFormattedAmount = Number(formattedAmount);
-    console.log('here is the type of numericFormattedAmount: ', typeof numericFormattedAmount);
     if (numericFormattedAmount * Number(this.split()) < Number(this.total())){
       numericFormattedAmount = (numericFormattedAmount + 0.01).toFixed(2);
       formattedAmount = numericFormattedAmount;
     }
     return formattedAmount;
   }, this);
-
-
   this.tipAmount = ko.computed(function(){
     var unformattedTipAmount = this.amount() * this.tipPercent() * 0.01;
     unformattedTipAmount = (Math.round(unformattedTipAmount*100))/100;
